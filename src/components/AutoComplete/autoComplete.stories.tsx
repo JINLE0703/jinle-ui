c08d27207e08193c31f56de1b57619d2226c66f7
Jinle <jinle0703@foxmail.com>
2021-02-13 12:10:42 +0800

    optimize autocomplete

--- src/components/AutoComplete/autoComplete.stories.tsx
+++ src/components/AutoComplete/autoComplete.stories.tsx
@@ -1,20 +1,48 @@
 import React from 'react';
 import { Meta } from '@storybook/react/types-6-0';
 
-import AutoComplete from './autoComplete';
+import AutoComplete, { DataSourceType } from './autoComplete';
 
 export default {
   title: '数据录入/AutoComplete',
   component: AutoComplete,
 } as Meta;
 
+interface lakerPlayersProps {
+  value: string;
+  number?: number;
+}
+
 export const DefaultAutoComplete: React.VFC<{}> = () => {
   const lakers = ['bradley', 'pope', 'caruso', 'cook', 'cousins', 'james', 'AD', 'green', 'howard', 'kuzma', 'McGee', 'rando'];
+  const lakersWithNumber = [
+    { value: 'bradley', number: 11 },
+    { value: 'pope', number: 1 },
+    { value: 'caruso', number: 4 },
+    { value: 'cook', number: 2 },
+    { value: 'cousins', number: 15 },
+    { value: 'james', number: 23 },
+    { value: 'AD', number: 3 },
+    { value: 'green', number: 14 },
+    { value: 'howard', number: 39 },
+    { value: 'kuzma', number: 0 },
+  ];
+  // const handleFetch = (str: string) => {
+  //   return lakers.filter((name) => name.includes(str)).map((item) => ({ value: item }));
+  // };
   const handleFetch = (str: string) => {
-    return lakers.filter((name) => name.includes(str));
+    return lakersWithNumber.filter((item) => item.value.includes(str));
   };
-  const handleSelected = (item: string) => {
-    console.log(item)
-  } 
-  return <AutoComplete fetchSuggestions={handleFetch} onSelect={handleSelected} />;
+  const handleSelected = (item: DataSourceType) => {
+    console.log(item);
+  };
+  const renderOption = (item: DataSourceType<lakerPlayersProps>) => {
+    return (
+      <>
+        <p>player: {item.value}</p>
+        <p>number: {item.number}</p>
+      </>
+    );
+  };
+  return <AutoComplete fetchSuggestions={handleFetch} onSelect={handleSelected} renderOption={renderOption} />;
 };
--- src/components/AutoComplete/autoComplete.tsx
+++ src/components/AutoComplete/autoComplete.tsx
@@ -1,18 +1,25 @@
 import React, { useState } from 'react';
 import Input, { InputProps } from '../Input/input';
 
+interface DataSourceObject {
+  value: string;
+}
+export type DataSourceType<T = {}> = T & DataSourceObject;
+
 export interface AutoCompleteProps extends Omit<InputProps, 'onSelect'> {
   /**触发自动补全的回调函数 */
-  fetchSuggestions: (str: string) => string[];
+  fetchSuggestions: (str: string) => DataSourceType[];
   /**点击下拉框任意一项回调函数 */
-  onSelect?: (item: string) => void;
+  onSelect?: (item: DataSourceType) => void;
+  /**自定义下拉项结构 */
+  renderOption?: (item: DataSourceType) => React.ReactElement;
 }
 
 const AutoComplete: React.FC<AutoCompleteProps> = (props) => {
-  const { fetchSuggestions, onSelect, value, ...restProps } = props;
+  const { fetchSuggestions, onSelect, value, renderOption, ...restProps } = props;
 
   const [inputValue, setInputValue] = useState(value);
-  const [suggestions, setSuggestions] = useState<string[]>([]);
+  const [suggestions, setSuggestions] = useState<DataSourceType[]>([]);
 
   /**
    * 处理 input value 改变事件
@@ -32,12 +39,20 @@ const AutoComplete: React.FC<AutoCompleteProps> = (props) => {
    * 处理点击下拉框内容事件
    * @param item 选中内容
    */
-  const handleSelected = (item: string) => {
-    setInputValue(item);
+  const handleSelected = (item: DataSourceType) => {
+    setInputValue(item.value);
     setSuggestions([]);
     if (onSelect) {
       onSelect(item);
     }
+  };
+
+  /**
+   * 处理下拉项结构模板
+   * @param item 
+   */
+  const renderTemplate = (item: DataSourceType) => {
+    return renderOption ? renderOption(item) : item.value;
   };
 
   /**
@@ -49,7 +64,7 @@ const AutoComplete: React.FC<AutoCompleteProps> = (props) => {
         {suggestions.map((item, index) => {
           return (
             <li key={index} onClick={() => handleSelected(item)}>
-              {item}
+              {renderTemplate(item)}
             </li>
           );
         })}
